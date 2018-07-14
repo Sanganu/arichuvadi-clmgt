e46b4d0ba75b06372a036962cddfb3b7e515f450
scodingjs <anukppkms@gmail.com>
2018-07-14 14:58:15 -0600

    searchdesc

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -88,7 +88,7 @@ class Searchstudents extends Component{
             <h4>Search Student Details </h4>
             <form className="inputsection">
                   <div className = "form-group row">
-                               <label forhtml = "searchstring" id ="lsearchstr">Search By Student Name / Email / Parentname  </label><br />
+                         <label forhtml = "searchstring" id ="lsearchstr">Search By Subject / Batch Description/ Student Name / Email / Parentname  </label><br />
                          <input className="textarea" onChange  = {this.handleInputChange} type="text" name="searchstring"  value={this.state.searchstring} /><br />.
                          <button name = "searchbtn" onClick = {this.searchrecords}>Search</button>
                   </div>
