66d0108108fa023c8c09580a4629968b128568c8
Sanganu <kppkmsanu@gmail.com>
2019-04-23 10:13:54 -0600

    Remove google align components

--- client/src/pages/Resources.js
+++ client/src/pages/Resources.js
@@ -13,7 +13,7 @@ const Resources = () => {
                         <Menubar />
                     </div>
                     <div className="col-lg-11 col-md-12 col-sm-12">
-                       <h4 className = "subhead">Resources</h4>
+                       <h3 className = "reshead">Resources</h3>
                        <ul className="list-unstyled list-group">
                            <li className="list-group-item">
                            <a href ="http://www.tamilvu.org/en/text-books" target="_blank" rel="noopener noreferrer"
@@ -23,8 +23,12 @@ const Resources = () => {
                            >University - Online Games</a></li>
                            <li className="list-group-item">
                            <a href ="https://www.google.com/intl/ta/inputtools/try/" target="_blank" rel="noopener noreferrer"
-                           >Online Writing Tool</a></li>
+                           >Online Google - Writing Tool</a></li>
+                            <li className="list-group-item">
+                           <a href ="https://sketch.io/sketchpad/" target="_blank" rel="noopener noreferrer"
+                           >Online Sketchpad - If you have touch screen, try this</a></li>
                         </ul>   
+                            <h5 className = "subheading">Basic Level - Reference Book</h5>
                             <embed src="ukgbook.pdf" 
                         type="application/pdf" width="95%" height="95%" /> 
                     </div>
